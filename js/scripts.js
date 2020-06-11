43c69791f02155ede88430f309145c1e3f5c7237
Sebastian Pidek <ajuc00@gmail.com>
2020-06-11 11:41:51 +0200

    Refactor settings into map, prepare for export to FoundryVTT

--- js/scripts.js
+++ js/scripts.js
@@ -45,30 +45,97 @@ function load(name) {
 	document.getElementById(name).value = window.localStorage.getItem(name);
 }
 
+function getParameterNames() {
+	return [
+		"gridType",
+		"gridSize",
+		"gridOpacity",
+		"width",
+		"height",
+		"seed",
+		"treeDensity",
+		"stoneDensity",
+		"twigsDensity",
+		"riverSize",
+		"centerRandomness",
+		"leavedTreeProportion",
+		"treeSize",
+		"treeColor",
+		"treeSeparation",
+		"serrationAmplitude",
+		"serrationFrequency",
+		"serrationRandomness",
+		"colorRandomness",
+		"clearings",
+		"clearingSize",
+		"treeSteps",
+		"backgroundNo"
+	];
+}
+
+function getParameterDefaultValue(name) {
+	const defaults = {
+		"gridType": -1,
+		"gridSize": 20,
+		"gridOpacity": 30,
+		"width": 1920,
+		"height": 1080,
+		"seed": 1,
+		"treeDensity": 40,
+		"stoneDensity": 40,
+		"twigsDensity": 40,
+		"riverSize": 2,
+		"centerRandomness": 30,
+		"leavedTreeProportion": 95,
+		"treeSize": 35,
+		"treeColor": 120,
+		"treeSeparation": 60,
+		"serrationAmplitude": 130,
+		"serrationFrequency": 30,
+		"serrationRandomness": 250,
+		"colorRandomness": 30,
+		"clearings": 9,
+		"clearingSize": 30,
+		"treeSteps": 3,
+		"backgroundNo": 3
+	};
+	return defaults[name];
+}
+
+function getParameterRandomizeFunction(name) {
+	const functions = {
+		"gridType": null,
+		"gridSize": null,
+		"gridOpacity": null,
+		"width": null,
+		"height": null,
+		"seed": function () { return Math.round(Math.random() * 65536); },
+		"treeDensity": function () { return Math.round(Math.random() * 100); },
+		"stoneDensity": function () { return Math.round(Math.random() * 20 * Math.random() * 5); },
+		"twigsDensity": function () { return Math.round(Math.random() * 20 * Math.random() * 5); },
+		"riverSize": function () { return Math.random() > 0.5 ? Math.round(Math.random() * 10) : 0; },
+		"centerRandomness": function () { return Math.round(30); },
+		"leavedTreeProportion": function () { return Math.round(Math.random() * 100); },
+		"treeSize": function () { return Math.round(20) + Math.round(Math.random() * 20); },
+		"treeColor": function () { return Math.round(Math.random() * 65536); },
+		"treeSeparation": function () { return Math.round(80 + Math.random() * 20); },
+		"serrationAmplitude": function () { return Math.round(80 + Math.random() * 40); },
+		"serrationFrequency": function () { return Math.round(80 + Math.random() * 40); },
+		"serrationRandomness": function () { return Math.round(100); },
+		"colorRandomness": function () { return Math.round(20); },
+		"clearings": function () { return Math.round(3 + Math.random() * 10); },
+		"clearingSize": function () { return Math.round(30 + Math.random() * 20); },
+		"treeSteps": function () { return Math.round(3 + Math.random() * 2); },
+		"backgroundNo": null
+	};
+	return functions[name];
+}
+
 function saveParametersToLocalStorage() {
-	save("gridType");
-	save("gridSize");
-	save("gridOpacity");
-	save("width");
-	save("height");
-	save("seed");
-	save("treeDensity");
-	save("stoneDensity");
-	save("twigsDensity");
-	save("riverSize");
-	save("centerRandomness");
-	save("leavedTreeProportion");
-	save("treeSize");
-	save("treeColor");
-	save("treeSeparation");
-	save("serrationAmplitude");
-	save("serrationFrequency");
-	save("serrationRandomness");
-	save("colorRandomness");
-	save("clearings");
-	save("clearingSize");
-	save("treeSteps");
-	save("backgroundNo");
+	var parameterNames = getParameterNames();
+	for(var parameterName of parameterNames) {
+		save(parameterName);
+	}
 	var defaultsWereSaved = window.localStorage.getItem("initialized");
 	if (!defaultsWereSaved)
 		window.localStorage.setItem("initialized", true);
@@ -80,29 +147,10 @@ function loadParametersFromLocalStorage() {
 		resetParameters();
 		return;
 	}
-	load("gridType");
-	load("gridSize");
-	load("gridOpacity");
-	load("width");
-	load("height");
-	load("seed");
-	load("treeDensity");
-	load("stoneDensity");
-	load("twigsDensity");
-	load("riverSize");
-	load("centerRandomness");
-	load("leavedTreeProportion");
-	load("treeSize");
-	load("treeColor");
-	load("treeSeparation");
-	load("serrationAmplitude");
-	load("serrationFrequency");
-	load("serrationRandomness");
-	load("colorRandomness");
-	load("clearings");
-	load("clearingSize");
-	load("treeSteps");
-	load("backgroundNo");
+	var parameterNames = getParameterNames();
+	for(var parameterName of parameterNames) {
+		load(parameterName);
+	}
 	backgroundChanged(document.getElementById("backgroundNo").value);
 }
 
@@ -112,6 +160,36 @@ function backgroundChanged(backgroundNo) {
 	document.image.src = "gfx/pattern_"+backgroundNo+".png";
 	//document.image.crossOrigin = "Anonymous";
 }
+
+function createExportTemplate(mapOriginInTiles, mapSizeInTiles, pixelsPerTile) {
+	return {
+		"format": "0.0.2",
+		"resolution": {
+			"map_origin": {
+				"x": mapOriginInTiles[0],
+				"y": mapOriginInTiles[1]
+			},
+			"map_size": {
+				"x": mapSizeInTiles[0],
+				"y": mapSizeInTiles[1]
+			},
+			"pixels_per_grid": pixelsPerTile
+		},
+		"line_of_sight": [],
+		"portals": [],
+		"environment": {
+			"baked_lighting": true,
+			"ambient_light": "ffffffff"
+		},
+		"lights": [],
+		"image": ""
+	};
+}
+
+function exportDd2vtt() {
+	var exportObject = createExportTemplate([0,0], [48, 48], 64);
+}
+
 function addListeners() {
 	document.getElementById("resetParameters").addEventListener('click', (event) => {
 		resetParameters();
@@ -121,6 +199,9 @@ function addListeners() {
 	});
 	document.getElementById("generate").addEventListener('click', (event) => {
 		location.reload();
+	});
+	document.getElementById("exportDd2vtt").addEventListener('click', (event) => {
+		exportDd2vtt();
 	});
 	// document.getElementById("download").addEventListener('click', (event) => {
 		// var canvas = document.getElementById("canvas");
@@ -146,26 +227,12 @@ function addListeners() {
 		save("height");
 		document.redrawNeeded = true;
 	});
-	addListener("gridType");
-	addListener("gridSize");
-	addListener("gridOpacity");
-	addListener("seed");
-	addListener("treeDensity");
-	addListener("stoneDensity");
-	addListener("twigsDensity");
-	addListener("riverSize");
-	addListener("centerRandomness");
-	addListener("leavedTreeProportion");
-	addListener("treeSize");
-	addListener("treeColor");
-	addListener("treeSeparation");
-	addListener("serrationAmplitude");
-	addListener("serrationFrequency");
-	addListener("serrationRandomness");
-	addListener("colorRandomness");
-	addListener("clearings");
-	addListener("clearingSize");
-	addListener("treeSteps");
+	var parameterNames = getParameterNames();
+	for(var parameterName of parameterNames) {
+		if (parameterName != "width" && parameterName != "height") {
+			addListener(parameterName);
+		}
+	}
 	document.getElementById("backgroundNo").addEventListener('change', (event) => {
 		var backgroundNo = event.target.value;
 		backgroundChanged(backgroundNo);
@@ -177,58 +244,23 @@ function clearStorage() {
 }
 
 function resetParameters() {
-	document.getElementById("gridType").value = -1;
-	document.getElementById("gridSize").value = 20;
-	document.getElementById("gridOpacity").value = 30;
-	document.getElementById("width").value = 1920;
-	document.getElementById("height").value = 1080;
-	document.getElementById("seed").value = 1;
-	document.getElementById("treeDensity").value = 40;
-	document.getElementById("stoneDensity").value = 40;
-	document.getElementById("twigsDensity").value = 40;
-	document.getElementById("riverSize").value = 2;
-	document.getElementById("centerRandomness").value = 30;
-	document.getElementById("leavedTreeProportion").value = 95;
-	document.getElementById("treeSize").value = 35;
-	document.getElementById("treeColor").value = 120;
-	document.getElementById("treeSeparation").value = 60;
-	document.getElementById("serrationAmplitude").value = 130;
-	document.getElementById("serrationFrequency").value = 30;
-	document.getElementById("serrationRandomness").value = 250;
-	document.getElementById("colorRandomness").value = 30;
-	document.getElementById("clearings").value = 9;
-	document.getElementById("clearingSize").value = 30;
-	document.getElementById("treeSteps").value = 3;
-	document.getElementById("backgroundNo").value = 3;
+	var parameterNames = getParameterNames();
+	for(var parameterName of parameterNames) {
+		document.getElementById(parameterName).value = getParameterDefaultValue(parameterName);
+	}
 	backgroundChanged(document.getElementById("backgroundNo").value);
 	saveParametersToLocalStorage();
 	document.redrawNeeded = true;
 }
 
 function randomizeParameters() {
-	// document.getElementById("gridType").value = -1;
-	// document.getElementById("gridSize").value = 20;
-	// document.getElementById("gridOpacity").value = 1;
-	// document.getElementById("width").value = 1920;
-	// document.getElementById("height").value = 1080;
-	// document.getElementById("backgroundNo").value = 1;
-	// document.getElementById("treeColor").value = 100;
-	document.getElementById("seed").value = Math.round(Math.random() * 65536);
-	document.getElementById("treeDensity").value = Math.round(Math.random() * 100);
-	document.getElementById("stoneDensity").value = Math.round(Math.random() * 20 * Math.random() * 5);
-	document.getElementById("twigsDensity").value = Math.round(Math.random() * 20 * Math.random() * 5);
-	document.getElementById("riverSize").value = Math.random() > 0.5 ? Math.round(Math.random() * 10) : 0;
-	document.getElementById("centerRandomness").value = Math.round(30);
-	document.getElementById("leavedTreeProportion").value = Math.round(Math.random() * 100);
-	document.getElementById("treeSize").value = Math.round(20) + Math.round(Math.random() * 20);
-	document.getElementById("treeSeparation").value = Math.round(80 + Math.random() * 20);
-	document.getElementById("serrationAmplitude").value = Math.round(80 + Math.random() * 40);
-	document.getElementById("serrationFrequency").value = Math.round(80 + Math.random() * 40);
-	document.getElementById("serrationRandomness").value = Math.round(100);
-	document.getElementById("colorRandomness").value = Math.round(20);
-	document.getElementById("clearings").value = Math.round(3 + Math.random() * 10);
-	document.getElementById("clearingSize").value = Math.round(30 + Math.random() * 20);
-	document.getElementById("treeSteps").value = Math.round(3 + Math.random() * 2);
+	var parameterNames = getParameterNames();
+	for(var parameterName of parameterNames) {
+		var randomizeFunction = getParameterRandomizeFunction(parameterName);
+		if (randomizeFunction) {
+			document.getElementById(parameterName).value = randomizeFunction();
+		}
+	}
 	saveParametersToLocalStorage();
 	document.redrawNeeded = true;
 }
@@ -299,9 +331,9 @@ function drawTreeRounded(
 					serrationFrequencyMin, serrationFrequencyMax,
 					serrationRngFactorX, serrationRngFactorY,
 					colorRandomness, treeColor,
-					seed,
+					rng,
 					fillInnerStepsWithColor) {
-	var rng = createRNG(seed);
+	var seed = rng();
 	var stepNo = 0.0;
 	var angle = 0.0;
 	var angleStep = 360.0 / angleSteps;
@@ -317,7 +349,7 @@ function drawTreeRounded(
 	var stepInited, lastDrawn;
 	
 	var phase, phaseShift, phaseMultiplier;
-	for (stepNo = 0; stepNo < steps; stepNo += 1) {
+	for (stepNo = 0; stepNo < steps; stepNo ++) {
 		centerX = centerX + centerRandomness * (stepNo/steps) * (saw((seed + stepNo * 131.0) * 131.0));
 		centerY = centerY + centerRandomness * (stepNo/steps) * (saw((seed + stepNo * 131.0) * 151.0));
 		
@@ -488,7 +520,7 @@ function drawBackground(canvas, context, rng) {
     context.fillRect(0, 0, canvas.width, canvas.height);
 }
 
-function drawRiver(canvas, context, angles, midpoints, widths, serrationAmplitude, serrationFrequency, serrationRandomness, rng, outColliderCircles, fillOpacity) {
+function drawRiver(canvas, context, angles, midpoints, widths, serrationAmplitude, serrationFrequency, serrationRandomness, rng, outColliderCircles, fillOpacity, r, g, b) {
 	// console.assert(angles.length >= 2 && angles.length <= 3);
 	// console.assert(midpoints.length <= 3);
 	// console.assert(widths.length = angles.length);
@@ -542,9 +574,9 @@ function drawRiver(canvas, context, angles, midpoints, widths, serrationAmplitud
 	let bezierCurve = new BezierCurve(pointsForBezier, 4);
 	context.lineWidth = 1;
 	context.strokeStyle = rgba(0, 0, 0, 1.0);
-	var rr = rng()*16;
-	var gg = rng()*64;
-	var bb = 200+56*rng();
+	var rr = r + rng()*16;
+	var gg = g + rng()*64;
+	var bb = b + 56*rng();
 	var fillColor = rgba(rr, gg, bb, fillOpacity);
 	var fillColorInside = rgba(rr*0.8, gg*0.8, bb*0.7, fillOpacity);
 	
@@ -660,6 +692,12 @@ function drawTwigs(canvas, context, x0, y0, len, width, angle, fillOpacity, rng)
 	context.fill();
 }
 
+function callRngNTimesToBalancePaths(n) {
+	for (var i=0; i<n; i++) {
+		rng();
+	}
+}
+
 function run(dt, forceRedraw) {
 	window.requestAnimationFrame(run);
 	
@@ -703,6 +741,7 @@ function run(dt, forceRedraw) {
 	var clearings = document.getElementById("clearings").value;
 	var clearingSize = document.getElementById("clearingSize").value * 0.01;
 	var treeSteps = document.getElementById("treeSteps").value;
+	var stepsNo;
 	//rng();
 	var i=0;
 	var listOfCircles = [];
@@ -716,8 +755,8 @@ function run(dt, forceRedraw) {
 		var angles = [alpha, beta];
 		var midpoints = [{x:rng() * canvas.width, y:rng() * canvas.height}];
 		var widths = [Math.round(3*riverSize*(1+rng())), Math.round(3*riverSize*(1+rng()))];
-		
-		drawRiver(canvas, context, angles, midpoints, widths, serrationAmplitude, serrationFrequency, serrationRandomness, rng, listOfCircles, treeColor);
+
+		drawRiver(canvas, context, angles, midpoints, widths, serrationAmplitude, serrationFrequency, serrationRandomness, rng, listOfCircles, treeColor, 0, 0, 200);
 	}
 	
 	rng = createRNG(seed);
@@ -725,8 +764,11 @@ function run(dt, forceRedraw) {
 		var xs = rng() * canvas.width;
 		var ys = rng() * canvas.height;
 		var rs = 5 + rng() * 9;
-		if (!collidesWithPreviousTrees(listOfCircles, xs, ys, 1))
+		if (!collidesWithPreviousTrees(listOfCircles, xs, ys, 1)) {
 			drawStone(canvas, context, xs, ys, rs, rng, colorRandomness, treeColor);
+		} else {
+			callRngNTimesToBalancePaths(4);
+		}
 	}
 	
 	rng = createRNG(seed);
@@ -745,8 +787,11 @@ function run(dt, forceRedraw) {
 		let width = 1 + rng() * 3;
 		
 		let xt = rng() * canvas.width;
-		if (!collidesWithPreviousTrees(listOfCircles, xt, yt, 5))
-			drawTwigs(canvas, context, xt, yt, len, width, rng()*Math.PI*2, treeColor, rng)
+		if (!collidesWithPreviousTrees(listOfCircles, xt, yt, 5)) {
+			drawTwigs(canvas, context, xt, yt, len, width, rng()*Math.PI*2, treeColor, rng);
+		} else {
+			callRngNTimesToBalancePaths(4);
+		}
 	}
 
 	rng = createRNG(seed);
@@ -754,30 +799,39 @@ function run(dt, forceRedraw) {
 		x0 = rng() * canvas.width;
 		y0 = rng() * canvas.height;
 		r0 = treeSize * (1 + rng());
+		stepsNo = Math.round(treeSteps*(0.75+rng()));
 		if (!collidesWithPreviousTrees(listOfCircles, x0, y0, r0)) {
 			if (rng() > leavedTreeProportion) {
-				//var ignored = rng();
 				drawTreeRounded(
-					context, x0, y0, r0*2, centerRandomness, Math.round(treeSteps*(0.75+rng())), 120,
+					context, x0, y0, r0*2, centerRandomness, stepsNo, 120,
 					5*serrationAmplitude, 2*serrationAmplitude,
 					7*serrationFrequency, 4*serrationFrequency,
 					0.5*serrationRandomness, 0.14*serrationRandomness,
 					colorRandomness, treeColor,
-					rng(),
+					rng,
 					true
 				);
 				listOfCircles.push({x:x0, y:y0, r:r0*4*treeSeparation});
 			} else {
 				drawTreeRounded(
-					context, x0, y0, r0*2, centerRandomness, Math.round(treeSteps*(0.75+rng())), 120,
+					context, x0, y0, r0*2, centerRandomness, stepsNo, 120,
 					5*serrationAmplitude, 2*serrationAmplitude,
 					9*serrationFrequency, 4*serrationFrequency,
 					0.6*serrationRandomness, 0.24*serrationRandomness,
 					colorRandomness, treeColor,
-					rng(),
+					rng,
 					false
 				);
 				listOfCircles.push({x:x0, y:y0, r:r0*4*treeSeparation});
+			}
+		} else {
+			callRngNTimesToBalancePaths(6);
+			var j,k;
+			for (j=0; j<stepsNo; j++) {
+				callRngNTimesToBalancePaths(9);
+				for (k=0; k<360; k += (360.0/120.0)) {
+					callRngNTimesToBalancePaths(1);
+				}
 			}
 		}
 	}
